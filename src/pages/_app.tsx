37b067825d0e774e7519f1214d53cc9597a635d3
EyCiYo <alexchdn12@gmail.com>
2023-10-31 16:50:46 +0530

    init commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>MainProject</title>
+        <meta name='viewport' content='width=device-width, initial-scale=1'></meta>
+        <meta name='description'content='Trying leetclone for mainproject'/>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
